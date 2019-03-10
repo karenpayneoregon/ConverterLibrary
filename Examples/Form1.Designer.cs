0a232d0a94e4e671d89d0123c2fb3623692a9e97
Karen Payne <paynekaren@comcast.net>
2019-03-10 16:52:35 -0700

    Added windows form project to allow methods to be easily tried while the same could be done in another type of project such as web based which is harded to ensure it works well while a windows form project is easier for developers to try.

---
+++ Examples/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Examples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.integerParseExamplesButton1 = new System.Windows.Forms.Button();
+            this.integerTryParseExamplesButton1 = new System.Windows.Forms.Button();
+            this.integerTryParseOutVarExamplesButton1 = new System.Windows.Forms.Button();
+            this.stringArrayToIntArrayVersion1Button = new System.Windows.Forms.Button();
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton = new System.Windows.Forms.Button();
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton = new System.Windows.Forms.Button();
+            this.assertAllElementsCanBeConvertedButton = new System.Windows.Forms.Button();
+            this.assertAllElementsCanBeConvertedGetOffendersButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.assertBeforeConvertGetOffendersExtensionsButton1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // integerParseExamplesButton1
+            // 
+            this.integerParseExamplesButton1.Location = new System.Drawing.Point(18, 21);
+            this.integerParseExamplesButton1.Name = "integerParseExamplesButton1";
+            this.integerParseExamplesButton1.Size = new System.Drawing.Size(462, 23);
+            this.integerParseExamplesButton1.TabIndex = 0;
+            this.integerParseExamplesButton1.Text = "Integer Parse Example Throw exception";
+            this.integerParseExamplesButton1.UseVisualStyleBackColor = true;
+            this.integerParseExamplesButton1.Click += new System.EventHandler(this.integerParseExampleButton1_Click);
+            // 
+            // integerTryParseExamplesButton1
+            // 
+            this.integerTryParseExamplesButton1.Location = new System.Drawing.Point(18, 50);
+            this.integerTryParseExamplesButton1.Name = "integerTryParseExamplesButton1";
+            this.integerTryParseExamplesButton1.Size = new System.Drawing.Size(462, 23);
+            this.integerTryParseExamplesButton1.TabIndex = 1;
+            this.integerTryParseExamplesButton1.Text = "Integer TryParse Example pre-C# 7";
+            this.integerTryParseExamplesButton1.UseVisualStyleBackColor = true;
+            this.integerTryParseExamplesButton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // integerTryParseOutVarExamplesButton1
+            // 
+            this.integerTryParseOutVarExamplesButton1.Location = new System.Drawing.Point(18, 79);
+            this.integerTryParseOutVarExamplesButton1.Name = "integerTryParseOutVarExamplesButton1";
+            this.integerTryParseOutVarExamplesButton1.Size = new System.Drawing.Size(462, 23);
+            this.integerTryParseOutVarExamplesButton1.TabIndex = 2;
+            this.integerTryParseOutVarExamplesButton1.Text = "Integer TryParse Example C# 7";
+            this.integerTryParseOutVarExamplesButton1.UseVisualStyleBackColor = true;
+            this.integerTryParseOutVarExamplesButton1.Click += new System.EventHandler(this.integerTryParseOutVarExamplesButton1_Click);
+            // 
+            // stringArrayToIntArrayVersion1Button
+            // 
+            this.stringArrayToIntArrayVersion1Button.Location = new System.Drawing.Point(18, 137);
+            this.stringArrayToIntArrayVersion1Button.Name = "stringArrayToIntArrayVersion1Button";
+            this.stringArrayToIntArrayVersion1Button.Size = new System.Drawing.Size(462, 23);
+            this.stringArrayToIntArrayVersion1Button.TabIndex = 3;
+            this.stringArrayToIntArrayVersion1Button.Text = "String array to int array version 1";
+            this.stringArrayToIntArrayVersion1Button.UseVisualStyleBackColor = true;
+            this.stringArrayToIntArrayVersion1Button.Click += new System.EventHandler(this.stringArrayToIntArrayVersion1Button_Click);
+            // 
+            // stringArrayToIntArrayVersion2ThrowsExceptionButton
+            // 
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.Location = new System.Drawing.Point(18, 166);
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.Name = "stringArrayToIntArrayVersion2ThrowsExceptionButton";
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.Size = new System.Drawing.Size(462, 23);
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.TabIndex = 4;
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.Text = "String array to int array version 2 Throws exception";
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.UseVisualStyleBackColor = true;
+            this.stringArrayToIntArrayVersion2ThrowsExceptionButton.Click += new System.EventHandler(this.stringArrayToIntArrayVersion2ThrowsExceptionButton_Click);
+            // 
+            // stringArrayToIntArrayVersion2NoExceptionButBadButton
+            // 
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.Location = new System.Drawing.Point(18, 195);
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.Name = "stringArrayToIntArrayVersion2NoExceptionButBadButton";
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.Size = new System.Drawing.Size(462, 23);
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.TabIndex = 5;
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.Text = "String array to int array version 2 No Exception but bad";
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.UseVisualStyleBackColor = true;
+            this.stringArrayToIntArrayVersion2NoExceptionButBadButton.Click += new System.EventHandler(this.stringArrayToIntArrayVersion2NoExceptionButBadButton_Click);
+            // 
+            // assertAllElementsCanBeConvertedButton
+            // 
+            this.assertAllElementsCanBeConvertedButton.Location = new System.Drawing.Point(9, 11);
+            this.assertAllElementsCanBeConvertedButton.Name = "assertAllElementsCanBeConvertedButton";
+            this.assertAllElementsCanBeConvertedButton.Size = new System.Drawing.Size(462, 23);
+            this.assertAllElementsCanBeConvertedButton.TabIndex = 6;
+            this.assertAllElementsCanBeConvertedButton.Text = "Assert before attempting conversion on array";
+            this.assertAllElementsCanBeConvertedButton.UseVisualStyleBackColor = true;
+            this.assertAllElementsCanBeConvertedButton.Click += new System.EventHandler(this.assertAllElementsCanBeConvertedButton_Click);
+            // 
+            // assertAllElementsCanBeConvertedGetOffendersButton
+            // 
+            this.assertAllElementsCanBeConvertedGetOffendersButton.Location = new System.Drawing.Point(9, 40);
+            this.assertAllElementsCanBeConvertedGetOffendersButton.Name = "assertAllElementsCanBeConvertedGetOffendersButton";
+            this.assertAllElementsCanBeConvertedGetOffendersButton.Size = new System.Drawing.Size(462, 23);
+            this.assertAllElementsCanBeConvertedGetOffendersButton.TabIndex = 7;
+            this.assertAllElementsCanBeConvertedGetOffendersButton.Text = "Assert before attempting conversion on array to offenders";
+            this.assertAllElementsCanBeConvertedGetOffendersButton.UseVisualStyleBackColor = true;
+            this.assertAllElementsCanBeConvertedGetOffendersButton.Click += new System.EventHandler(this.assertAllElementsCanBeConvertedGetOffendersButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(462, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Assert before attempting conversion on array to offenders";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.assertBeforeConvertGetOffendersExtensionsButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.assertAllElementsCanBeConvertedGetOffendersButton);
+            this.groupBox1.Controls.Add(this.assertAllElementsCanBeConvertedButton);
+            this.groupBox1.Location = new System.Drawing.Point(18, 246);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(489, 161);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // assertBeforeConvertGetOffendersExtensionsButton1
+            // 
+            this.assertBeforeConvertGetOffendersExtensionsButton1.Location = new System.Drawing.Point(9, 98);
+            this.assertBeforeConvertGetOffendersExtensionsButton1.Name = "assertBeforeConvertGetOffendersExtensionsButton1";
+            this.assertBeforeConvertGetOffendersExtensionsButton1.Size = new System.Drawing.Size(462, 23);
+            this.assertBeforeConvertGetOffendersExtensionsButton1.TabIndex = 9;
+            this.assertBeforeConvertGetOffendersExtensionsButton1.Text = "Using Extensions";
+            this.assertBeforeConvertGetOffendersExtensionsButton1.UseVisualStyleBackColor = true;
+            this.assertBeforeConvertGetOffendersExtensionsButton1.Click += new System.EventHandler(this.assertBeforeConvertGetOffendersExtensionsButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.stringArrayToIntArrayVersion2NoExceptionButBadButton);
+            this.Controls.Add(this.stringArrayToIntArrayVersion2ThrowsExceptionButton);
+            this.Controls.Add(this.stringArrayToIntArrayVersion1Button);
+            this.Controls.Add(this.integerTryParseOutVarExamplesButton1);
+            this.Controls.Add(this.integerTryParseExamplesButton1);
+            this.Controls.Add(this.integerParseExamplesButton1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code examples";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button integerParseExamplesButton1;
+        private System.Windows.Forms.Button integerTryParseExamplesButton1;
+        private System.Windows.Forms.Button integerTryParseOutVarExamplesButton1;
+        private System.Windows.Forms.Button stringArrayToIntArrayVersion1Button;
+        private System.Windows.Forms.Button stringArrayToIntArrayVersion2ThrowsExceptionButton;
+        private System.Windows.Forms.Button stringArrayToIntArrayVersion2NoExceptionButBadButton;
+        private System.Windows.Forms.Button assertAllElementsCanBeConvertedButton;
+        private System.Windows.Forms.Button assertAllElementsCanBeConvertedGetOffendersButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button assertBeforeConvertGetOffendersExtensionsButton1;
+    }
+}
+
---
+++ Examples/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConverterLibrary;
+
+namespace Examples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Throws an exception as an empty string can't be converted to an int.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void integerParseExampleButton1_Click(object sender, EventArgs e)
+        {
+            string someStringValue = "";
+            int result = int.Parse(someStringValue);
+            Console.WriteLine(result);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string someStringValue = "";
+            int result = 0;
+
+            if (int.TryParse(someStringValue, out result))
+            {
+                MessageBox.Show($"'{someStringValue}' converted");
+            }
+            else
+            {
+                MessageBox.Show($"'{someStringValue}' not converted");
+            }
+
+            someStringValue = "12";
+
+            if (int.TryParse(someStringValue, out result))
+            {
+                MessageBox.Show($"'{someStringValue}' converted, value * 2 is {result *2}");
+            }
+            else
+            {
+                MessageBox.Show($"'{someStringValue}' not converted");
+            }
+        }
+
+        private void integerTryParseOutVarExamplesButton1_Click(object sender, EventArgs e)
+        {
+            string someStringValue = "";
+
+            if (int.TryParse(someStringValue, out var result1))
+            {
+                MessageBox.Show($"'{someStringValue}' converted");
+            }
+            else
+            {
+                MessageBox.Show($"'{someStringValue}' not converted");
+            }
+
+            someStringValue = "12";
+
+            if (int.TryParse(someStringValue, out var result2))
+            {
+                MessageBox.Show($"'{someStringValue}' converted, value * 2 is  {result2 * 2}");
+            }
+            else
+            {
+                MessageBox.Show($"'{someStringValue}' not converted");
+            }
+        }
+        /// <summary>
+        /// Common solution found on the web
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stringArrayToIntArrayVersion1Button_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4", "6", "8" };
+            int[] intArray = Array.ConvertAll(stringArray, int.Parse);
+
+            MessageBox.Show(string.Join(",", intArray));
+        }
+
+        private void stringArrayToIntArrayVersion2ThrowsExceptionButton_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4B", "6", "8" };
+            int[] intArray = Array.ConvertAll(stringArray, int.Parse);
+
+            MessageBox.Show(string.Join(",", intArray));
+        }
+        /// <summary>
+        /// C# 7
+        /// When a element can not be converted 0 is used in it's place
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stringArrayToIntArrayVersion2NoExceptionButBadButton_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4B", "6", "8" };
+            var intArray = Array.ConvertAll(stringArray, item =>  
+                int.TryParse(item, out var intResult) ? intResult : 0);
+
+            MessageBox.Show(string.Join(",", intArray));
+
+        }
+
+        private void assertAllElementsCanBeConvertedButton_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4B", "6", "8" };
+            if (stringArray.SelectMany(item => item.ToCharArray()).All(char.IsNumber))
+            {
+                var intArray = Array.ConvertAll(stringArray, item =>
+                    int.TryParse(item, out var intResult) ? intResult : 0);
+
+                MessageBox.Show(string.Join(",", intArray));
+            }
+            else
+            {
+                MessageBox.Show("One or more elements can not be converted");
+            }
+        }
+        /// <summary>
+        /// See which elements in the string array can not be converted to integers.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void assertAllElementsCanBeConvertedGetOffendersButton_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4B", "6", "8A" };
+            if (stringArray.SelectMany(item => item.ToCharArray()).All(char.IsNumber))
+            {
+                var intArray = Array.ConvertAll(stringArray, item =>
+                    int.TryParse(item, out var intResult) ? intResult : 0);
+
+                MessageBox.Show(string.Join(",", intArray));
+            }
+            else
+            {
+                var msg = "One or more elements can not be converted because of the following element indexes\n";
+
+                MessageBox.Show($"{msg}{string.Join(", ", stringArray.GetNonIntegerIndexes())}");
+            }
+        }
+
+        private void assertBeforeConvertGetOffendersExtensionsButton1_Click(object sender, EventArgs e)
+        {
+            string[] stringArray = { "2", "4B", "6", "8A" };
+
+            if (stringArray.AllInt())
+            {
+                MessageBox.Show(string.Join(",", stringArray.ToIntegerPreserveArray()));
+            }
+            else
+            {
+                var msg = "One or more elements can not be converted because of the following element indexes\n";
+
+                MessageBox.Show($"{msg}{string.Join(", ", stringArray.GetNonIntegerIndexes())}");
+                MessageBox.Show($"But these did convert '{string.Join(",", stringArray.ToIntegerArray())}'");
+            }
+
+
+            stringArray = new[] { "2", "4", "6", "8" };
+
+            if (stringArray.AllInt())
+            {
+                MessageBox.Show(string.Join(",", stringArray.ToIntegerPreserveArray()));
+            }
+            else
+            {
+                var msg = "One or more elements can not be converted because of the following element indexes\n";
+
+                MessageBox.Show($"{msg}{string.Join(", ", stringArray.GetNonIntegerIndexes())}");
+            }
+
+
+
+        }
+
+    }
+}
